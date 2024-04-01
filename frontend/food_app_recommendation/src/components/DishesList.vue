b7d72689167894d13336c0539a11d632d57bc825
ValentinaAlvarezC <158078455+ValentinaAlvarezC@users.noreply.github.com>
2024-03-31 22:02:34 -0700

    Completed recommended list component using mood and cuisine id

--- frontend/food_app_recommendation/src/components/DishesList.vue
+++ frontend/food_app_recommendation/src/components/DishesList.vue
@@ -1,40 +1,46 @@
 <template>
     <div class="dish-list">
-        <!-- <div>
-            <h1>Recommended List of dishes</h1>
-        </div>
-        <div class="list">
-            <div class="dish-image">
-                <img :src="dish.imageUrl" alt="Dish Image" />
-            </div>
-            <div>
-                <h2>Dish Name: </h2>
-                <div>
-                    <p>Description:</p>
-                </div>
-                <router-link to="/RecommendedDish">
-                    <button>See recipe</button>
-                </router-link>
-            </div>
 
-        </div> -->
-
+        <h1>Recommended List of Dishes</h1>
+        <p>Here's a recommended list of dishes for you! </p>
         <div class="test-table">
             <table>
                 <thead>
                     <tr>
+                        <th>Image</th>
                         <th>Dish</th>
                         <th>Description</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="dish in dishList" :key="dish.dishId">
+
                         <td>{{ dish.dishName }}</td>
                         <td>{{ dish.description }}</td>
+                        <td>
+                            <router-link to="/RecommendedDish">
+                                <button>See recipe</button>
+                            </router-link>
+                        </td>
+                    </tr>
+                    <tr v-for="dish in dishList2" :key="dish.dishId">
+
+                        <td>{{ dish.dishName }}</td>
+                        <td>{{ dish.description }}</td>
+                        <td>
+                            <router-link to="/RecommendedDish">
+                                <button>See recipe</button>
+                            </router-link>
+                        </td>
+
                     </tr>
                 </tbody>
             </table>
         </div>
+        <br>
+
+
 
 
 
@@ -47,45 +53,60 @@ export default {
     name: 'dishesList',
     data() {
         return {
-            // dish: {
-            //     imageUrl: 'https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_50,w_1900/hellofresh_s3/image/HF_Y24_R07_W08_ES_ESQFC18619-2_edit_zuccini_Main_R_high-137f2c4e.jpg',
-            //     name: 'Name of the dish', // Get name here
-            //     description: '', // Get description here
-            // }
+            dish: {
+                imageUrl: 'https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_50,w_1900/hellofresh_s3/image/HF_Y24_R07_W08_ES_ESQFC18619-2_edit_zuccini_Main_R_high-137f2c4e.jpg'
+
+            },
             moodId: 0,
             dishList: [],
+            dishList2: [],
+
         };
     },
 
-    methods:{
-        getDishesFromMood(){
+    methods: {
+        getDishesFromMood() {
             const moodId = this.$route.params.moodId;
+
             DishListService.getDishesFromMood(moodId)
                 .then(response => {
                     this.dishList = response.data;
-                    console.log(this.dishList)
+                    console.log(this.dishList);
                 })
                 .catch(error => {
                     console.error("Error fetching dishes:", error);
                 })
-        }
-        // getUserPreferences(userId) {
-        //     UserPreferenceService.getUserPreferencesByUserId(userId)
-        //         .then(response => {
-        //             this.userPreferences = response.data;
-        //             console.log(response.data);
-        //         })
-        //         .catch(error => {
-        //             console.error('Error fetching user preferences:', error);
-        //         });
-        // },
-    },
+        },
+        getDishesFromCuisin() {
+            const cuisineId = this.$route.params.cuisineId;
 
-    mounted(){
-        
+            DishListService.getDishesFromCuisine(cuisineId)
+                .then(response => {
+                    this.dishList2 = response.data;
+                    console.log(this.dishList2);
+                })
+                .catch(error => {
+                    console.error("Error fetching dishes:", error);
+                })
+
+
+        },
+
+
+
+
+
+    },
+    mounted() {
+
+        const moodId = this.$route.params.moodId;
         const cuisineId = this.$route.params.cuisineId;
         this.getDishesFromMood();
+        this.getDishesFromCuisin();
         console.log("Cuisine ID:", cuisineId);
+        console.log("Mood ID:", moodId);
+
+
     }
 
 };
--- frontend/food_app_recommendation/src/services/DishListService.js
+++ frontend/food_app_recommendation/src/services/DishListService.js
@@ -4,6 +4,9 @@ class DishListService {
     getDishesFromMood(moodId) {
         return http.get(`/moods/${moodId}/dishes`);
     }
+    getDishesFromCuisine(cuisineId){
+        return http.get(`/cuisines/${cuisineId}/dishes`)
+    }
 }
 
 export default new DishListService();
